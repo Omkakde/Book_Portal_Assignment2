0adc60bda77c5a439442fea404c0cc5c11b79033
Omkakde <kakadeom123@gmail.com>
2024-10-03 15:48:14 +0530

    added UC4 new Feature

--- BookPortal.js
+++ BookPortal.js
@@ -18,7 +18,8 @@ function menu() {
     1) show available books
     2) add book
     3) show cart
-    4) Exit`);
+    4) update cart
+    5) Exit`);
         let input = readline.question("    Enter option :");
         switch (input) {
             case '1':
@@ -31,6 +32,9 @@ function menu() {
                 showCart();
                 break;
             case '4':
+                updateCart();
+                break;
+            case '5':
                 console.log("Exiting the program. Goodbye!");
                 return;
             default:
@@ -65,7 +69,7 @@ function AddBookToCart() {
         return;
     } else {
         const quantity = parseInt(readline.question("    Enter Quantity: "), 10);
-        if ( quantity <= 0 || quantity > booksList[input].quantity) {
+        if (isNaN(quantity) || quantity <= 0 || quantity > booksList[input].quantity) {
             console.log(`Invalid quantity. Please enter a value between 1 and ${booksList[input].quantity}.`);
             return;
         }
@@ -74,7 +78,6 @@ function AddBookToCart() {
 
         if (booksList[input].quantity === 0) {
             booksList[input].status = "unavailable";
-            console.log("unavailable");    // UC3
         }
 
         let cartBook = {
@@ -91,21 +94,92 @@ function AddBookToCart() {
 }
 
 function showCart() {
+    let idx=0;
     console.log(`                          *** My Cart ****             
-        +------------------------+-------------+-------------+-------------+
-        | Book                   | Price       | Quantity    | Total Price |
-        +------------------------+-------------+-------------+-------------+`);
+        +---------+------------------------+-------------+-------------+-------------+
+        |  Index  | Book                   | Price       | Quantity    | Total Price |
+        +---------+------------------------+-------------+-------------+-------------+`);
     let totalCartValue = 0;
 
     cart.forEach(book => {
         let totalPrice = book.price * book.quantity;
         console.log(`          
-        | ${book.book_name}|    ${book.price}      |    ${book.quantity}        |    ${totalPrice}       |`);
+        | ${idx}       | ${book.book_name}|    ${book.price}      |    ${book.quantity}        |    ${totalPrice}       |`);
         totalCartValue += totalPrice;
+        idx++;
     });
 
     console.log(`\nTotal Cart Value: ${totalCartValue}`);
 }
+
+
+function updateCart() {
+    if (cart.length === 0) {
+        console.log("Cart is empty.");
+        return;
+    } else {
+        console.log(`
+        1) Increase quantity
+        2) Decrease quantity
+        3) Remove 
+        4) exit
+        Enter option No.:`);
+        
+        const input = readline.question("    Enter option: ");
+        showCart();
+
+        const bookIdx = parseInt(readline.question("    Enter book Index: "), 10);
+        if (bookIdx < 0 || bookIdx >= cart.length) {
+            console.log("Invalid index. Please try again.");
+            return;
+        }
+
+        const increaseQuantity = parseInt(readline.question("    Enter quantity: "), 10);
+
+        switch (input) {
+            case '1':
+                if (increaseQuantity > 0 && booksList[bookIdx].quantity >= increaseQuantity) {
+                    cart[bookIdx].quantity += increaseQuantity;
+                    booksList[bookIdx].quantity -= increaseQuantity;
+                    console.log(`Increased quantity of "${cart[bookIdx].book_name}" by ${increaseQuantity}.`);
+                } else {
+                    console.log("Invalid quantity. Unable to increase.");
+                }
+                break;
+
+            case '2':
+                if (increaseQuantity > 0 && cart[bookIdx].quantity >= increaseQuantity) {
+                    cart[bookIdx].quantity -= increaseQuantity;
+                    booksList[bookIdx].quantity += increaseQuantity;
+                    if (cart[bookIdx].quantity === 0) {
+                        const removedBook = cart[bookIdx].book_name; 
+                        cart.splice(bookIdx, 1);
+                        console.log(`Book "${removedBook}" removed from cart because quantity is zero.`);
+                    } else { 
+                        console.log(`Decreased quantity of "${cart[bookIdx].book_name}" by ${increaseQuantity}.`);
+                    }
+                } else {
+                    console.log("Invalid quantity. Unable to decrease.");
+                }
+                break;
+
+            case '3':
+                const removedBook = cart[bookIdx].book_name; 
+                cart.splice(bookIdx, 1); 
+                console.log(`Book "${removedBook}" removed from cart.`);
+                break;
+                
+            case '4':
+                return;
+
+            default:
+                console.log("Invalid option. Please try again.");
+        }
+    }
+}
+
+
+
 
 menu();
  
